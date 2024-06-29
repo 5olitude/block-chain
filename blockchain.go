cec21e601a07be0a99f46595a56d4f41f9e647b6
5olitude <josephhermis2020@cs.sjcetpalai.ac.in>
2024-06-29 10:39:34 +0530

    three blocks

--- blockchain.go
+++ blockchain.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"strings"
 	"time"
 )
 
@@ -14,20 +15,44 @@ type Block struct {
 }
 
 func NewBlock(nonce int, previousHash string) *Block {
-
 	b := new(Block)
 	b.nonce = nonce
 	b.previousHash = previousHash
 	b.timestamp = time.Now().UnixNano()
 	return b
-
 }
 
 func (b *Block) Print() {
-	fmt.Printf("timestamp		   %d\n", b.timestamp)
-	fmt.Printf("nonce		       %d\n", b.nonce)
-	fmt.Printf("previous_hash	   %s\n", b.previousHash)
-	fmt.Printf("transactions		   %s\n", b.transactions)
+	fmt.Printf("timestamp%d\n", b.timestamp)
+	fmt.Printf("nonce%d\n", b.nonce)
+	fmt.Printf("previous_hash%s\n", b.previousHash)
+	fmt.Printf("transactions%s\n", b.transactions)
+}
+
+type Blockchain struct {
+	transactionPool []string
+	chain           []*Block
+}
+
+func NewBlockchain() *Blockchain {
+	bc := new(Blockchain)
+	bc.chain = []*Block{}          // Initialize chain as empty slice of Block pointers
+	bc.CreateBlock(0, "init hash") // Create genesis block
+	return bc
+}
+
+func (bc *Blockchain) CreateBlock(nonce int, previousHash string) *Block {
+	b := NewBlock(nonce, previousHash)
+	bc.chain = append(bc.chain, b) // Append new block to chain
+	return b
+}
+
+func (bc *Blockchain) Print() {
+	for i, block := range bc.chain {
+		fmt.Printf("%s Chain %d  %s\n", strings.Repeat("=", 25), i, strings.Repeat("=", 25))
+		block.Print()
+	}
+	fmt.Printf("%s\n", strings.Repeat("*", 25))
 }
 
 func init() {
@@ -35,6 +60,13 @@ func init() {
 }
 
 func main() {
-	b := NewBlock(0, "init hash")
-	b.Print()
+	blockChain := NewBlockchain()
+	blockChain.Print()
+
+	// Add blocks to the blockchain
+	blockChain.CreateBlock(5, "hash 1")
+	blockChain.Print()
+
+	blockChain.CreateBlock(2, "hash 2")
+	blockChain.Print()
 }
