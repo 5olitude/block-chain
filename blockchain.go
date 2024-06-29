079f106b8610a718ae519f6ca9094a84a79a7e89
5olitude <josephhermis2020@cs.sjcetpalai.ac.in>
2024-06-29 11:27:53 +0530

    created  blocks hash

--- blockchain.go
+++ blockchain.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/json"
 	"fmt"
 	"log"
 	"strings"
@@ -9,12 +11,12 @@ import (
 
 type Block struct {
 	nonce        int
-	previousHash string
+	previousHash [32]byte
 	timestamp    int64
 	transactions []string
 }
 
-func NewBlock(nonce int, previousHash string) *Block {
+func NewBlock(nonce int, previousHash [32]byte) *Block {
 	b := new(Block)
 	b.nonce = nonce
 	b.previousHash = previousHash
@@ -25,8 +27,27 @@ func NewBlock(nonce int, previousHash string) *Block {
 func (b *Block) Print() {
 	fmt.Printf("timestamp%d\n", b.timestamp)
 	fmt.Printf("nonce%d\n", b.nonce)
-	fmt.Printf("previous_hash%s\n", b.previousHash)
+	fmt.Printf("previous_hash%x\n", b.previousHash)
 	fmt.Printf("transactions%s\n", b.transactions)
+}
+
+func (b *Block) Hash() [32]byte {
+	m, _ := json.Marshal(b)
+	fmt.Println(string(m))
+	return sha256.Sum256([]byte(m))
+}
+func (b *Block) MarshalJSON() ([]byte, error) {
+	return json.Marshal(struct {
+		Timestamp    int64    `json:"timestamp"`
+		Nonce        int      `json:"nonce"`
+		PreviousHash [32]byte `json:"previous_hash"`
+		Transactions []string `json:"transactions"`
+	}{
+		Timestamp:    b.timestamp,
+		Nonce:        b.nonce,
+		PreviousHash: b.previousHash,
+		Transactions: b.transactions,
+	})
 }
 
 type Blockchain struct {
@@ -35,16 +56,21 @@ type Blockchain struct {
 }
 
 func NewBlockchain() *Blockchain {
+	b := &Block{}
 	bc := new(Blockchain)
-	bc.chain = []*Block{}          // Initialize chain as empty slice of Block pointers
-	bc.CreateBlock(0, "init hash") // Create genesis block
+	bc.chain = []*Block{}       // Initialize chain as empty slice of Block pointers
+	bc.CreateBlock(0, b.Hash()) // Create genesis block
 	return bc
 }
 
-func (bc *Blockchain) CreateBlock(nonce int, previousHash string) *Block {
+func (bc *Blockchain) CreateBlock(nonce int, previousHash [32]byte) *Block {
 	b := NewBlock(nonce, previousHash)
 	bc.chain = append(bc.chain, b) // Append new block to chain
 	return b
+}
+
+func (bc *Blockchain) LastBlock() *Block {
+	return bc.chain[len(bc.chain)-1]
 }
 
 func (bc *Blockchain) Print() {
@@ -60,13 +86,17 @@ func init() {
 }
 
 func main() {
+
 	blockChain := NewBlockchain()
 	blockChain.Print()
 
+	previousHash := blockChain.LastBlock().Hash()
 	// Add blocks to the blockchain
-	blockChain.CreateBlock(5, "hash 1")
+	blockChain.CreateBlock(5, previousHash)
+	blockChain.Print()
+	
+	previousHash = blockChain.LastBlock().Hash()
+	blockChain.CreateBlock(2, previousHash)
 	blockChain.Print()
 
-	blockChain.CreateBlock(2, "hash 2")
-	blockChain.Print()
 }
