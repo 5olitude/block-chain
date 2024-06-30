da873d3ae4ab32128fff4986ffe5e8effb671b12
5olitude <josephhermis2020@cs.sjcetpalai.ac.in>
2024-06-30 13:31:13 +0530

    feat:generated the ecdsa private and public key

--- block/blockchain.go
+++ block/blockchain.go
@@ -1,4 +1,4 @@
-package main
+package block
 
 import (
 	"crypto/sha256"
@@ -178,27 +178,4 @@ func (t *Transaction) MarshalJSON() ([]byte, error) {
 		Recipient: t.recipientBlockchainAddress,
 		Value:     t.value,
 	})
-}
-
-func init() {
-	log.SetPrefix("BlockChain :")
-}
-
-func main() {
-	myBlockchainAddress := "my_block_address"
-	blockChain := NewBlockchain(myBlockchainAddress)
-	blockChain.Print()
-
-	blockChain.AddTransaction("A", "B", 1.0)
-	blockChain.Mining()
-	blockChain.Print()
-
-	blockChain.AddTransaction("C", "D", 2.0)
-	blockChain.AddTransaction("X", "Y", 3.0)
-	blockChain.Mining()
-	blockChain.Print()
-	fmt.Printf("C %.1f\n", blockChain.CalculateTotalAmount("C"))
-	fmt.Printf("D %.1f\n", blockChain.CalculateTotalAmount("D"))
-	fmt.Printf("my_reward %.1f\n", blockChain.CalculateTotalAmount("my_block_address"))
-
 }
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"block-chain/wallet"
+	"fmt"
+	"log"
+)
+
+func init() {
+	log.SetPrefix("BlockChain :")
+}
+
+func main() {
+	w := wallet.NewWallet()
+	fmt.Println(w.PrivateKeyStr())
+	fmt.Println(w.PublicKeyStr())
+}
